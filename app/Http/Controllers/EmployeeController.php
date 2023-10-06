657e5b6f86cfe58c1134ccaa63d0424f4121a3fc
Ronald Emata Latina <ronald.emata.latina@gmail.com>
2023-10-06 14:36:08 +0800

    New Update

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class EmployeeController extends Controller
 {
@@ -28,11 +29,79 @@ class EmployeeController extends Controller
                 </tr>           
             </thead>
             <tbody>';
+            foreach ($employee as $rs) {
+                $output .= '<tr>
+                <td>' . $rs->id . '</td>
+                <td><img src="storage/images/' . $rs->avatar . '" width="50" class="img-thumbnail rounded-circle"></td>
+                <td>' . $rs->first_name . ' ' . $rs->last_name . '</td>
+                <td>' . $rs->email . '</td>
+                <td>
+                <a href="#" id="' . $rs->id . '" class="text-success mx-1 editIcon" data-bs-toggle="modal" data-bs-target="#editEmployeeModal"><i class="bi-pencil-square h4"></i></a>
+                <a href="#" id="' . $rs->id . '" class="text-danger mx-1 deleteIcon"><i class="bi-trash h4"></i></a>
+                </td>
+                </tr>';
+            }
 
             $output .= '</tbody></table>';
             echo $output;
         } else {
             echo '<h1 class="text-center text-secondary my-5">No record in the database!</h1>';
+        }
+    }
+
+    // insert a new employee ajax request
+    public function store(Request $request)
+    {
+        $file = $request->file('avatar');
+        $fileName = time() . '.' . $file->getClientOriginalExtension();
+        $file->storeAs('public/images', $fileName); //php artisan storage:link
+
+        $empData = ['first_name' => $request->fname, 'last_name' => $request->lname, 'email' => $request->email, 'avatar' => $fileName];
+        Employee::create($empData);
+        return response()->json([
+            'status' => 200,
+        ]);
+    }
+
+    //edit an employee ajax request
+    public function edit(Request $request)
+    {
+        $id = $request->id;
+        $emp = Employee::find($id);
+        return response()->json($emp);
+    }
+
+    // update an employee ajax request
+    public function update(Request $request)
+    {
+        $fileName = '';
+        $emp = Employee::find($request->emp_id);
+        if ($request->hasFile('avatar')) {
+            $file = $request->file('avatar');
+            $fileName = time() . '.' . $file->getClientOriginalExtension();
+            $file->storeAs('public/images', $fileName);
+            if ($emp->avatar) {
+                Storage::delete('public/images/' . $emp->avatar);
+            }
+        } else {
+            $fileName = $request->emp_avatar;
+        }
+
+        $empData = ['first_name' => $request->fname, 'last_name' => $request->lname, 'email' => $request->email, 'avatar' => $fileName];
+
+        $emp->update($empData);
+        return response()->json([
+            'status' => 200,
+        ]);
+    }
+
+    // delete an employee ajax request
+    public function delete(Request $request)
+    {
+        $id = $request->id;
+        $emp = Employee::find($id);
+        if (Storage::delete('public/images/' . $emp->avatar)) {
+            Employee::destroy($id);
         }
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel 10 CRUD with Image Upload using jQuery Ajax with SweetAlert and DataTables</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel='stylesheet'
         href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css' />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" />
 </head>
 
 <body>
@@ -33,7 +35,7 @@
         </div>
     </div>
 
-    <!-- Modal -->
+    <!-- Add Employee Modal -->
     <div class="modal fade" id="addEmployeeModal" tabindex="-1" aria-labelledby="addEmployeeModal" aria-hidden="true"
         data-bs-backdrop="static">
         <div class="modal-dialog modal-dialog-centered">
@@ -42,13 +44,82 @@
                     <h5 class="modal-title" id="F">Add New Employee</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body p-4 bg-light">
-                    Form
+                <form action="#" method="POST" id="add_employee_form" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-body p-4 bg-light">
+                        <div class="row">
+                            <div class="col-lg">
+                                <label for="fname">First Name</label>
+                                <input type="text" name="fname" class="form-control" placeholder="First Name"
+                                    required>
+                            </div>
+                            <div class="col-lg">
+                                <label for="lname">Last Name</label>
+                                <input type="text" name="lname" class="form-control" placeholder="Last Name"
+                                    required>
+                            </div>
+                        </div>
+                        <div class="my-2">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+                        </div>
+                        <div class="my-2">
+                            <label for="avatar">Select Avatar</label>
+                            <input type="file" name="avatar" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" id="add_employee_btn" class="btn btn-primary">Add Employee</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Edit Employee Modal -->
+    <div class="modal fade" id="editEmployeeModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+        data-bs-backdrop="static" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Edit Employee</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" id="add_employee_btn" class="btn btn-primary">Add Employee</button>
-                </div>
+                <form action="#" method="POST" id="edit_employee_form" enctype="multipart/form-data">
+                    @csrf
+                    <input type="hidden" name="emp_id" id="emp_id">
+                    <input type="hidden" name="emp_avatar" id="emp_avatar">
+                    <div class="modal-body p-4 bg-light">
+                        <div class="row">
+                            <div class="col-lg">
+                                <label for="fname">First Name</label>
+                                <input type="text" name="fname" id="fname" class="form-control"
+                                    placeholder="First Name" required>
+                            </div>
+                            <div class="col-lg">
+                                <label for="lname">Last Name</label>
+                                <input type="text" name="lname" id="lname" class="form-control"
+                                    placeholder="Last Name" required>
+                            </div>
+                        </div>
+                        <div class="my-2">
+                            <label for="email">E-mail</label>
+                            <input type="email" name="email" id="email" class="form-control"
+                                placeholder="E-mail" required>
+                        </div>
+                        <div class="my-2">
+                            <label for="avatar">Select Avatar</label>
+                            <input type="file" name="avatar" class="form-control">
+                        </div>
+                        <div class="mt-2" id="avatar"></div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="submit" id="edit_employee_btn" class="btn btn-success">Update
+                            Employee</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
@@ -57,9 +128,131 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
 
     <script>
         $(function() {
+
+            // add new employee ajax request
+            $('#add_employee_form').submit(function(e) {
+                e.preventDefault();
+                const fd = new FormData(this);
+                $("#add_employee_btn").text('Adding...');
+                $.ajax({
+                    url: '{{ route('store') }}',
+                    method: 'post',
+                    data: fd,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    dataType: 'json',
+                    success: function(response) {
+                        if (response.status == 200) {
+                            Swal.fire(
+                                'Added!',
+                                'Employee Added Successfully',
+                                'success'
+                            )
+                            fetchAllEmployees();
+                        }
+                        $("#add_employee_btn").text('Add Employee');
+                        $("#add_employee_form")[0].reset();
+                        $("#addEmployeeModal").modal('hide');
+                    }
+                });
+            });
+
+            //edit employee ajax ajax request
+            $(document).on('click', '.editIcon', function(e) {
+                e.preventDefault();
+                let id = $(this).attr('id'); //get the value of attribute id
+                // alert(id);
+                $.ajax({
+                    url: '{{ route('edit') }}',
+                    method: 'get',
+                    data: {
+                        id: id,
+                        _token: '{{ csrf_token() }}'
+                    },
+                    success: function(response) {
+                        $("#fname").val(response.first_name);
+                        $("#lname").val(response.last_name);
+                        $("#email").val(response.email);
+                        $("#avatar").html(
+                            `<img src="storage/images/${response.avatar}" width="100" class="img-fluid img-thumbnail">`
+                        );
+                        $("#emp_id").val(response.id);
+                        $("#emp_avatar").val(response.avatar);
+                    }
+                });
+            });
+
+            //update employee ajax request
+            $("#edit_employee_form").submit(function(e) {
+                e.preventDefault();
+                const fd = new FormData(this);
+                $("#edit_employee_btn").text('Updating...');
+                $.ajax({
+                    url: '{{ route('update') }}',
+                    method: 'post',
+                    data: fd,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    dataType: 'json',
+                    success: function(response) {
+                        if (response.status == 200) {
+                            Swal.fire(
+                                'Updated!',
+                                'Employee Updated Successfully',
+                                'success'
+                            )
+                            fetchAllEmployees();
+                        }
+                        $("#edit_employee_btn").text('Update Employee');
+                        $("#edit_employee_form")[0].reset();
+                        $("#editEmployeeModal").modal('hide');
+                    }
+                });
+            });
+
+            // delete employee ajax request
+            $(document).on('click', '.deleteIcon', function(e) {
+                e.preventDefault();
+                let id = $(this).attr('id');
+                let csrf = '{{ csrf_token() }}';
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        $.ajax({
+                            url: '{{ route('delete') }}',
+                            method: 'delete',
+                            data: {
+                                id: id,
+                                _token: csrf
+                            },
+                            success: function(response) {
+                                console.log(response);
+                                Swal.fire(
+                                    'Deleted!',
+                                    'Your file has been deleted.',
+                                    'success'
+                                )
+                                fetchAllEmployees();
+                            }
+                        });
+                    }
+                })
+            });
+
             //fetch all employee ajax request
             fetchAllEmployees();
 
@@ -69,6 +262,9 @@
                     method: 'get',
                     success: function(response) {
                         $("#show_all_employees").html(response);
+                        $("table").DataTable({
+                            order: [0, 'desc']
+                        });
                     }
                 });
             }
